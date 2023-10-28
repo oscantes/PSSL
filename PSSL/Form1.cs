919275e376be2b7503c8e2ca171adbabb9720efc
B7LWBTO <B7LWBTO@NB341127C2020>
2023-10-28 16:25:31 +0300

    additional info about git revert&reset

--- PSSL/Form1.cs
+++ PSSL/Form1.cs
@@ -26,5 +26,10 @@ namespace PSSL
             MessageBox.Show($"Hello {usernamebox.Text}, you can use your computer {computernamebox.Text}");
         }
         //made a change to test reverting back to a previous version
+        //git revert is different from git reset
+        //when revert is done, it doesn't edit history but adds to it
+        //plus it changes files to the reverted branch and you can sync project right after
+        //reset however deletes history and reverts branch to the desired one
+        //additionally "git push -f" has to be done from vs > git > cmd otherwise you can't push project to remote
     }
 }
